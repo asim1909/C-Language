d9484eca5260b1ecda68a754ff45cfecdbbedec8
出久 <118390636+asim1909@users.noreply.github.com>
2023-02-09 12:29:54 +0530

    Create User-Input.c

---
+++ C-programizpro/User-Input.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() 
+{
+    char Name[30];
+    printf("Type your name: ");
+    scanf("%s", &Name);
+    printf("Hello %s", Name);
+}
