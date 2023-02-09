a35a836f45e926399657dc48c5046f6ae3c72d1e
出久 <118390636+asim1909@users.noreply.github.com>
2023-02-09 12:03:36 +0530

    Create printf_function.c

---
+++ C-programizpro/printf_function.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+// void main() 
+int main()
+{
+    int a;
+    a = printf("Hello deku");
+    printf("\n%ld",a);
+    printf("\n%d",sizeof(a));
+//     return 0;  
+    return 0;
+}
